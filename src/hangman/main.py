689897a3b095d3852f2649b534bbbac24c6716a5
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-02-25 15:45:54 +0100

    Stage 4 completed - unittest.mock

--- src/hangman/main.py
+++ src/hangman/main.py
@@ -7,14 +7,18 @@ def print_title():
     print('H A N G M A N')
 
 
-def random_word() -> str:
+def random_word(seed: int | None) -> str:
+    if seed:
+        random.seed(seed)
     return random.choice(WORDS)
 
 
-def main():
+def main(seed=None):
     print_title()
-    guess = input('Guess the word: ')
-    print('You survived!' if guess == random_word() else 'You lost!')
+    to_guess = random_word(seed)
+    hidden = to_guess[:3] + '-' * (len(to_guess) - 3)
+    guess = input(f'Guess the word {hidden}: ')
+    print('You survived!' if guess == to_guess else 'You lost!')
 
 
 if __name__ == '__main__':
--- test/test_main.py
+++ test/test_main.py
@@ -1,20 +1,37 @@
 from io import StringIO
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import unittest
 
-from hangman.main import main, random_word, WORDS
+from hangman.main import main, random_word
 
 
 class HangmanTest(unittest.TestCase):
 
     def test_random_word(self):
-        self.assertIn(random_word(), WORDS)
+        self.assertEquals(random_word(1), 'java')
+        self.assertEquals(random_word(44), 'javascript')
+        self.assertEquals(random_word(55), 'python')
+        self.assertEquals(random_word(333), 'swift')
 
     @patch('builtins.input')
     @patch('sys.stdout', new_callable=StringIO)
-    def test_stage3_win_or_loose(self, mock_stdout: StringIO, mock_input):
+    def test_stage4_win(self, mock_stdout: StringIO, mock_input):
         mock_args = ['java']
         mock_input.side_effect = mock_args
-        main()
-        self.assertTrue(mock_stdout.getvalue().find('You survived!') >= 0 or
-                        mock_stdout.getvalue().find('You lost!') >= 0)
+        main(seed=1)
+        self.assertTrue(mock_stdout.getvalue().find('You survived!') >= 0)
+
+    @patch('builtins.input')
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_stage4_loose(self, mock_stdout: StringIO, mock_input):
+        mock_args = ['java']
+        mock_input.side_effect = mock_args
+        main(seed=55)
+        self.assertTrue(mock_stdout.getvalue().find('You lost!') >= 0)
+
+    @patch('builtins.input')
+    def test_stage4_hidden_word(self, mock_input: MagicMock):
+        main(seed=55)
+        mock_input.assert_called_once_with('Guess the word pyt---: ')
+        main(seed=44)
+        mock_input.assert_called_with('Guess the word jav-------: ')
