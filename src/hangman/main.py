330a1c4a823bc34497fe924c658faf36e43e65c6
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-02-25 14:49:15 +0100

    Stage 3 completed - first test

--- src/hangman/main.py
+++ src/hangman/main.py
@@ -1,11 +1,20 @@
+import random
+
+WORDS = ['python', 'java', 'swift', 'javascript']
+
+
 def print_title():
     print('H A N G M A N')
+
+
+def random_word() -> str:
+    return random.choice(WORDS)
 
 
 def main():
     print_title()
     guess = input('Guess the word: ')
-    print('You survived!' if guess == 'python' else 'You lost!')
+    print('You survived!' if guess == random_word() else 'You lost!')
 
 
 if __name__ == '__main__':
--- test/test_main.py
+++ test/test_main.py
@@ -2,23 +2,19 @@ from io import StringIO
 from unittest.mock import patch
 import unittest
 
-from hangman.main import main
+from hangman.main import main, random_word, WORDS
 
 
 class HangmanTest(unittest.TestCase):
 
-    @patch('builtins.input')
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_stage2_win_on_right_guess(self, mock_stdout: StringIO, mock_input):
-        mock_args = ['python']
-        mock_input.side_effect = mock_args
-        main()
-        assert mock_stdout.getvalue().find('You survived!') >= 0
+    def test_random_word(self):
+        self.assertIn(random_word(), WORDS)
 
     @patch('builtins.input')
     @patch('sys.stdout', new_callable=StringIO)
-    def test_stage2_loose_on_wrong_guess(self, mock_stdout: StringIO, mock_input):
-        mock_args = ['wrong']
+    def test_stage3_win_or_loose(self, mock_stdout: StringIO, mock_input):
+        mock_args = ['java']
         mock_input.side_effect = mock_args
         main()
-        assert mock_stdout.getvalue().find('You lost!') >= 0
+        self.assertTrue(mock_stdout.getvalue().find('You survived!') >= 0 or
+                        mock_stdout.getvalue().find('You lost!') >= 0)
