5bc03c91725a2b78c4b48c8642600b8ca943c23e
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-02-26 20:56:31 +0100

    Final stage implementation with tests

--- src/hangman/main.py
+++ src/hangman/main.py
@@ -9,6 +9,8 @@ class Hangman:
         self.word_to_guess: str = self.random_word(seed=seed)
         self.letters_asked: set[str] = set()
         self.attempts_left = 8
+        self.wins = 0
+        self.losses = 0
         
     @staticmethod
     def print_title():
@@ -21,19 +23,32 @@ class Hangman:
 
     def do_guessing(self) -> bool:
         while self.attempts_left:
-            guess = input(f'\n{self.get_progress()}\nInput a letter: ')
-            self.check_guess(guess)
+            self.handle_guess()
             if self.get_progress() == self.word_to_guess:
-                print(f'\n{self.word_to_guess}\nYou guessed the word!')
+                print(f'You guessed the word {self.word_to_guess}!')
+                self.wins += 1
                 return True
+        self.losses += 1
         return False
 
-    def check_guess(self, guess: str):
-        if guess not in self.word_to_guess:
+    def handle_guess(self):
+        guess = self.get_valid_guess()
+        if guess in self.letters_asked:
+            print("You've already guessed this letter.")
+        elif guess not in self.word_to_guess:
             self.handle_failed_attempt("That letter doesn't appear in the word.")
-        elif guess in self.letters_asked:
-            self.handle_failed_attempt('No improvements.')
         self.letters_asked.add(guess)
+
+    def get_valid_guess(self) -> str:
+        valid_input = False
+        while not valid_input:
+            guess = input(f'\n{self.get_progress()}\nInput a letter: ')
+            if len(guess) != 1:
+                print('Please, input a single letter.')
+            elif not guess.islower():
+                print('Please, enter a lowercase letter from the English alphabet.')
+            else:
+                return guess
 
     def handle_failed_attempt(self, message: str):
         self.attempts_left -= 1
@@ -42,10 +57,30 @@ class Hangman:
     def get_progress(self):
         return ''.join(map(lambda c: c if c in self.letters_asked else '-', self.word_to_guess))
 
-    def main(self):
-        self.print_title()
+    def play(self):
         guessed = self.do_guessing()
         print('You survived!' if guessed else '\nYou lost!')
+        self.reset_game()
+
+    def results(self):
+        print(f'You won: {self.wins} times.')
+        print(f'You lost: {self.losses} times.')
+
+    def reset_game(self):
+        self.word_to_guess = self.random_word(None)
+        self.attempts_left = 8
+        self.letters_asked = set()
+
+    def main(self):
+        self.print_title()
+        choice = ''
+        while choice != 'exit':
+            choice = input('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit:')
+            match choice:
+                case 'play':
+                    self.play()
+                case 'results':
+                    self.results()
 
 
 if __name__ == '__main__':
--- test/test_main.py
+++ test/test_main.py
@@ -23,25 +23,53 @@ class HangmanTest(unittest.TestCase):
 
     @patch('builtins.input')
     @patch('sys.stdout', new_callable=StringIO)
-    def test_stage6_do_guessing_win(self, mock_stdout: StringIO, mock_input: MagicMock):
-        mock_args = ['j', 'i', 'g', 'g', 'g', 'g', 'a', 'v']
+    def test_get_valid_guess(self, mock_stdout: StringIO, mock_input: MagicMock):
+        mock_args = ['j-', 'ai', '', 'G', 'g']
+        mock_input.side_effect = mock_args
+        self.assertEqual('g', Hangman().get_valid_guess())
+        self.assertEqual(3, mock_stdout.getvalue().count('Please, input a single letter.'))
+        self.assertEqual(1, mock_stdout.getvalue().count('Please, enter a lowercase letter from the English alphabet.'))
+
+    @patch('builtins.input')
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_stage7_example1_loose(self, mock_stdout: StringIO, mock_input: MagicMock):
+        mock_args = ['a', 'i', 'o', 'o', 'p', 'p', 'h', 'k', 'a', 'z', 't', 'x', 'b', 'd', 'w']
+        mock_input.side_effect = mock_args
+        self.assertFalse(Hangman(seed=44).do_guessing())
+        self.assertEqual(8, mock_stdout.getvalue().count("That letter doesn't appear in the word."))
+        self.assertEqual(3, mock_stdout.getvalue().count("You've already guessed this letter."))
+        mock_input.assert_any_call("\n----------\nInput a letter: ")
+        mock_input.assert_any_call("\n-a-a------\nInput a letter: ")
+        mock_input.assert_any_call("\n-a-a---i--\nInput a letter: ")
+        mock_input.assert_any_call("\n-a-a---ip-\nInput a letter: ")
+        mock_input.assert_called_with("\n-a-a---ipt\nInput a letter: ")
+
+    @patch('builtins.input')
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_stage7_example2_win(self, mock_stdout: StringIO, mock_input: MagicMock):
+        mock_args = ['j', 'i', '+', 'A', 'ii', '++', '', 'g', 'a', 'v']
         mock_input.side_effect = mock_args
         self.assertTrue(Hangman(seed=1).do_guessing())
-        self.assertEqual(5, mock_stdout.getvalue().count("That letter doesn't appear in the word."))
-        self.assertEqual(0, mock_stdout.getvalue().count("No improvements."))
-        self.assertEqual(1, mock_stdout.getvalue().count("\njava\nYou guessed the word!"))
+        self.assertEqual(2, mock_stdout.getvalue().count("That letter doesn't appear in the word."))
+        self.assertEqual(3, mock_stdout.getvalue().count('Please, input a single letter.'))
+        self.assertEqual(2, mock_stdout.getvalue().count('Please, enter a lowercase letter from the English alphabet.'))
+        self.assertEqual(1, mock_stdout.getvalue().count("You guessed the word java!"))
         mock_input.assert_any_call("\nj---\nInput a letter: ")
         mock_input.assert_called_with("\nja-a\nInput a letter: ")
 
     @patch('builtins.input')
     @patch('sys.stdout', new_callable=StringIO)
-    def test_stage6_do_guessing_loose(self, mock_stdout: StringIO, mock_input: MagicMock):
-        mock_args = ['a', 'i', 'o', 'z', 'l', 'p', 'h', 'k', 'j', 'j', 'a', 'i']
+    def test_stage8(self, mock_stdout: StringIO, mock_input: MagicMock):
+        mock_args = ['play', 'h', 'K', 't', 'o', '+', 'K', 'h', 'K', 'n', 'q', 'y', 'h', 'p', 'results', 'exit']
         mock_input.side_effect = mock_args
-        self.assertFalse(Hangman(seed=44).do_guessing())
-        self.assertEqual(5, mock_stdout.getvalue().count("That letter doesn't appear in the word."))
-        self.assertEqual(3, mock_stdout.getvalue().count("No improvements."))
-        mock_input.assert_any_call("\n-a-a------\nInput a letter: ")
-        mock_input.assert_any_call("\n-a-a---i--\nInput a letter: ")
-        mock_input.assert_any_call("\n-a-a---ip-\nInput a letter: ")
-        mock_input.assert_called_with("\nja-a---ip-\nInput a letter: ")
+        Hangman(seed=55).main()
+        self.assertEqual(1, mock_stdout.getvalue().count("That letter doesn't appear in the word."))
+        self.assertEqual(4, mock_stdout.getvalue().count('Please, enter a lowercase letter from the English alphabet.'))
+        self.assertEqual(2, mock_stdout.getvalue().count("You've already guessed this letter."))
+        self.assertEqual(1, mock_stdout.getvalue().count("You guessed the word python!"))
+        self.assertEqual(1, mock_stdout.getvalue().count("You won: 1 times.\nYou lost: 0 times."))
+        mock_input.assert_any_call("\n---h--\nInput a letter: ")
+        mock_input.assert_any_call("\n--tho-\nInput a letter: ")
+        mock_input.assert_any_call("\n-ython\nInput a letter: ")
+        mock_input\
+            .assert_called_with('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit:')
